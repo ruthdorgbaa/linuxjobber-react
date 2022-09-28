c3dc4997c69612b02313661ff157a11e7e6c959d
ruthdorgbaa <ruthdorgbaa@gmail.com>
2022-09-28 15:05:14 +0100

    first commit

---
+++ src/components/nav/nav.js
@@ -0,0 +1,11 @@
+import {img} from './asset/logo.svg'
+
+export default function Nav() {
+    return (
+        <nav>
+            <div>
+                <img src={img} />
+            </div>
+        </nav>
+    )
+}
